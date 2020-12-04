f2dcbf539fd56ca5f35d564b4cc7b39d3261604c
Anastasiya Trembach <nastia.trembach@gmail.com>
2020-12-04 14:53:59 +0200

    add comments

--- Table.js
+++ Table.js
@@ -5,6 +5,7 @@ class Table {
 	static previousElement;
 	static previousValue;
 
+	// Створення таблиці
     constructor(name, data) {
 
 		this.filename = name;
@@ -20,29 +21,29 @@ class Table {
 		this.elemTable.appendChild(this.elemHeader); 
 		this.elemBody = document.createElement('tbody');
 		this.elemTable.appendChild(this.elemBody); 
-
 		this.createHeader(data.headers);
 		this.createRow(data.data);
     }
 
+	// Створення рядку заколовків таблиці
     createHeader(array) {  
 		const elemRow = document.createElement('tr');
 		this.elemHeader.appendChild(elemRow);
 
 		array.map( (value) => {
-		const elemCell = document.createElement('th');
-		elemCell.classList.add('sort');
+			const elemCell = document.createElement('th');
+			elemCell.classList.add('sort');
 			elemCell.innerHTML = value;
 			elemRow.appendChild(elemCell);
 		});    
     }
 
+	// Створення рядків таблиці
     createRow(array) {
 		for (let row of array) {
 			const elemRow = document.createElement('tr');
 			this.elemBody.appendChild(elemRow);
 			
-
 			for (let i = 0; i < this.elemHeader.children[0].children.length; i++) {
 				const elemCell = document.createElement('td');
 				elemCell.classList.add('modal-trigger');
@@ -51,94 +52,78 @@ class Table {
 				elemCell.innerHTML = row[i] || '';
 				elemRow.appendChild(elemCell);
 			}
-			
 		}
     }
 
+	// Сортування таблиці
     sortTableByColumn(column, asc = true) {
-        const dirModifier = asc ? 1 : -1;
+        const mod = asc ? 1 : -1; // Індитифікатор для сортування за прямим або оберненим порядком
         const rows = Array.from(this.elemBody.querySelectorAll("tr"));
         
-      
-        // Sort each row
+        // Сортування кожного рядка
         const sortedRows = rows.sort((a, b) => {
             let aCol = a.querySelector(`td:nth-child(${ column + 1 })`).textContent;
             let bCol = b.querySelector(`td:nth-child(${ column + 1 })`).textContent;
-      
             aCol = parseInt(aCol) || aCol.trim();
             bCol = parseInt(bCol) || bCol.trim();
-      
-            return aCol > bCol ? (1 * dirModifier) : (-1 * dirModifier);
+            return aCol > bCol ? (1 * mod) : (-1 * mod);
         });
       
-        // Remove all existing TRs from the table
+		// Видалення всіх рядків таблиці
         while (this.elemBody.firstChild) {
             this.elemBody.removeChild(this.elemBody.firstChild);
         }
-      
-        // Re-add the newly sorted rows
-        this.elemBody.append(...sortedRows);
-      
-        // Remember how the column is currently sorted
+        // Додавання відсортованих рядків
+		this.elemBody.append(...sortedRows);
+		
+		// Позначеняя колонки за якою зараз відсортовані дані
         this.elemTable.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
         this.elemTable.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-asc", asc);
         this.elemTable.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-desc", !asc);
     }
 
+	// Створення модального вінка
     openModal(element) {
-
 		const container = document.getElementById('modal-container');
-		
-		// переписать эту часть!
-		const elemModal1 = `<div id="modal" class="modal">
-				<div class="modal-content">
-					<h4>Modal Header</h4>
-					<input type="text" id="newText">
-				</div>
-				<div class="modal-footer">
-					<a href="#!" class="modal-close waves-effect waves-green btn-flat">Cancel</a>
-					<a class="modal-close waves-effect waves-green btn-flat" id="ok">OK</a>
-				</div>
-				</div>`;
-
 		const elemModal = document.createElement('div');
 		elemModal.classList.add('modal');
 		elemModal.id = 'modal';
-		container.appendChild(elemModal);
+		container.append(elemModal);
+
 		const modalContent = document.createElement('div');
 		modalContent.classList.add('modal-content');
-		elemModal.appendChild(modalContent);
+
 		const modalFooter = document.createElement('div');
 		modalFooter.classList.add('modal-footer');
-		elemModal.appendChild(modalFooter);
+		elemModal.append(modalContent, modalFooter);
 
 		const modalHeader = document.createElement('h4');
 		modalHeader.innerHTML = 'Change the cell';
-		modalContent.appendChild(modalHeader);
+
 		const modalInput = document.createElement('input');
 		modalInput.setAttribute('type', 'text');
-		modalContent.appendChild(modalInput);
+		modalContent.append(modalHeader, modalInput);
 
 		const cancelBtn = document.createElement('a');
 		cancelBtn.classList.add('modal-close', 'waves-effect','waves-green', 'btn-flat');
-		// cancelBtn.setAttribute('href', '#!');
 		cancelBtn.innerHTML = 'Cancel';
-		modalFooter.appendChild(cancelBtn);
+
 		const okBtn = document.createElement('a');
 		okBtn.classList.add('modal-close', 'waves-effect','waves-green', 'btn-flat');
 		okBtn.innerHTML = 'OK';
-		modalFooter.appendChild(okBtn);
+		modalFooter.append(cancelBtn, okBtn);
 		
-		//container.innerHTML = elemModal;
 		const modal = M.Modal.init(document.getElementById('modal'));
-		//const modal = M.Modal.init(elemModal);
-		//const newText = document.getElementById('newText');
 		modalInput.defaultValue = element.target.innerHTML;
-		//const okBtn = document.getElementById('ok');
 
+		// Обробник для кнопки CANCEL модального вінка
+		// Знищує модальне вікно без внесення змін в таблицю
 		cancelBtn.addEventListener('click', () => {
 			container.innerHTML = '';
 		});
+
+		// Обробник для кнопки SAVE модального вінка
+		// Знищує модальне вікно та зберігає файл
 		okBtn.addEventListener('click', () => {
 			
 			// тут this берется события, а не класса, поетому 
--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 'use strict';
 
-// добавить коментариев
-
 let newTable, newEditor;
 
-
+// Функція викликається при натисненні на кнопку OPEN, після вибору файлу
 function readFile(input) {
     let file = input.files[0];
-    let reader = new FileReader();
+	let reader = new FileReader();
+	
+	// Читання файлу
     reader.readAsText(file);
   
+	// Після завантаження файла, створюються екземпляри класів Editor i Table
     reader.onload = function() {
-    	newEditor = new Editor();
+		newEditor = new Editor();
 		const data = newEditor.parseFile(reader.result);
 		newTable = new Table(file.name, data);
 
+		// Обробник для сортавування даних в колонках
 		document.querySelectorAll(".sort").forEach(headerCell => {
 		headerCell.addEventListener("click", () => {
 				const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
@@ -24,6 +26,7 @@ function readFile(input) {
 			});
 		});
 
+		// Обробник для кнопки SAVE, збереження файлу
 		const saveBtn = document.getElementById('save');
 		saveBtn.addEventListener('click', function() {
 			const headers = newTable.elemHeader.querySelectorAll("th");
@@ -33,6 +36,7 @@ function readFile(input) {
 			openModal(filename, text);
 		}, false);
 		
+		// Обробник для кнопки UNDO, відміна останньої дії
 		const undoBtn = document.getElementById('undo');
 		undoBtn.addEventListener('click', function() {
 			const element = Table.previousElement;
@@ -41,6 +45,7 @@ function readFile(input) {
 
   	};
   
+	// Обробник для помилок, при читанні файлу
     reader.onerror = function() {
       	console.log(reader.error);
     };
@@ -48,46 +53,52 @@ function readFile(input) {
 }
 
 
+// Функція для відкриття модального вікна для вибору імені під яким зберегти файл
 function openModal(filename, text) {
-	console.log('zaschol');
 	let newFilename = '';
-	const container = document.getElementById('modal-container');
 
+	// Створення модального вінка та необхідних елементів для нього
+	const container = document.getElementById('modal-container');
 	const elemModal = document.createElement('div');
 	elemModal.classList.add('modal');
 	elemModal.id = 'modal';
-	container.appendChild(elemModal);
+	container.append(elemModal);
+
 	const modalContent = document.createElement('div');
 	modalContent.classList.add('modal-content');
-	elemModal.appendChild(modalContent);
+
 	const modalFooter = document.createElement('div');
 	modalFooter.classList.add('modal-footer');
-	elemModal.appendChild(modalFooter);
+	elemModal.append(modalContent, modalFooter);
 
 	const modalHeader = document.createElement('h4');
 	modalHeader.innerHTML = 'Save as';
-	modalContent.appendChild(modalHeader);
+
 	const modalInput = document.createElement('input');
 	modalInput.setAttribute('type', 'text');
-	modalContent.appendChild(modalInput);
+	modalContent.append(modalHeader, modalInput);
 
 	const cancelBtn = document.createElement('a');
 	cancelBtn.classList.add('modal-close', 'waves-effect','waves-green', 'btn-flat');
 	cancelBtn.innerHTML = 'Cancel';
-	modalFooter.appendChild(cancelBtn);
+
 	const saveBtn = document.createElement('a');
 	saveBtn.classList.add('modal-close', 'waves-effect','waves-green', 'btn-flat');
 	saveBtn.innerHTML = 'Save';
-	modalFooter.appendChild(saveBtn);
+	modalFooter.append(cancelBtn, saveBtn);
 	
 	const modal = M.Modal.init(document.getElementById('modal'));
 	modalInput.defaultValue = filename;
 	
+	// Обробник для кнопки CANCEL модального вінка
+	// Знищує модальне вікно без збереження файлу 
 	cancelBtn.addEventListener('click', () => {
 		container.innerHTML = '';
 	});
+
+	// Обробник для кнопки SAVE модального вінка
+	// Знищує модальне вікно та зберігає файл
 	saveBtn.addEventListener('click', () => {
-		
 		newFilename = modalInput.value || filename;
 		container.innerHTML = '';
 		newEditor.download(newFilename, text);
